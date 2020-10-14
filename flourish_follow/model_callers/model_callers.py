45cf5c4f6694041fa39c16b35c3d492a32fbe62a
ckgathi <ckgathi@users.noreply.github.com>
2020-10-14 11:53:37 +0200

    update model callers and templates

--- flourish_follow/model_callers/model_callers.py
+++ flourish_follow/model_callers/model_callers.py
@@ -1,6 +1,6 @@
 from edc_call_manager.model_caller import ModelCaller, DAILY
 from edc_call_manager.decorators import register
-from flourish_maternal.models import SubjectConsent, MaternalLocator
+from flourish_maternal.models import MaternalLocator
 from edc_call_manager.models import Call, Log, LogEntry
 
 from ..models import WorkList
@@ -10,7 +10,8 @@ from ..models import WorkList
 class WorkListFollowUpModelCaller(ModelCaller):
     call_model = (Call)
     locator_model = (MaternalLocator, 'subject_identifier')
-    consent_model = (SubjectConsent, 'subject_identifier')
+#     consent_model = (SubjectConsent, 'subject_identifier')
+    alternative_locator_filter = 'study_maternal_identifier'
     log_entry_model = LogEntry
     log_model = Log
     interval = DAILY
