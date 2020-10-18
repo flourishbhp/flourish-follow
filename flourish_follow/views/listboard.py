262177243ef6cdb7b011d4307fd5f994daa280d1
ckgathi <ckgathi@users.noreply.github.com>
2020-10-18 18:27:02 +0200

    add filters

--- flourish_follow/views/listboard.py
+++ flourish_follow/views/listboard.py
@@ -4,16 +4,17 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.utils.decorators import method_decorator
 from edc_base.view_mixins import EdcBaseViewMixin
-from edc_dashboard.view_mixins import ListboardFilterViewMixin, SearchFormViewMixin
+from edc_dashboard.view_mixins import SearchFormViewMixin
 from edc_dashboard.views import ListboardView
 from edc_navbar import NavbarViewMixin
 
 from ..model_wrappers import WorkListModelWrapper
+from .filters import ListboardViewFilters
 
 
 
 class ListboardView(NavbarViewMixin, EdcBaseViewMixin,
-                    ListboardFilterViewMixin, SearchFormViewMixin, ListboardView):
+                    ListboardViewFilters, SearchFormViewMixin, ListboardView):
 
     listboard_template = 'flourish_follow_listboard_template'
     listboard_url = 'flourish_follow_listboard_url'
@@ -21,6 +22,7 @@ class ListboardView(NavbarViewMixin, EdcBaseViewMixin,
     listboard_fa_icon = "fa-user-plus"
 
     model = 'flourish_follow.worklist'
+    listboard_view_filters = ListboardViewFilters()
     model_wrapper_cls = WorkListModelWrapper
     navbar_name = 'flourish_follow'
     navbar_selected_item = 'worklist'
