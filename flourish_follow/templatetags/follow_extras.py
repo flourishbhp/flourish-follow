55146d3a85f42936cac531802265eda9088f4667
Moses Chawawa <moseschawawa@gmail.com>
2022-03-14 15:19:22 +0200

    fixed the dashboard link

--- flourish_follow/templatetags/follow_extras.py
+++ flourish_follow/templatetags/follow_extras.py
@@ -11,8 +11,8 @@ def has_group(user, group_name):
 
 @register.inclusion_tag('flourish_follow/buttons/dashboard_button.html')
 def dashboard_button(model_wrapper):
-    child_dashboard_url = settings.DASHBOARD_URL_NAMES.get(
-        'child_dashboard_url')
+    subject_dashboard_url = settings.DASHBOARD_URL_NAMES.get(
+        'subject_dashboard_url')
     return dict(
-        child_dashboard_url=child_dashboard_url,
+        subject_dashboard_url=subject_dashboard_url,
         subject_identifier=model_wrapper.subject_identifier)
