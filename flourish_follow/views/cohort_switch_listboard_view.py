c41d378d233632c55f9add29572e412c61aee07a
Ame Diphoko <diphokoame@gmail.com>
2025-02-27 12:02:19 +0200

    add: FU limits for all the cohorts to disable further enrolments when limits reached

--- flourish_follow/views/cohort_switch_listboard_view.py
+++ flourish_follow/views/cohort_switch_listboard_view.py
@@ -42,6 +42,47 @@ class CohortSwitchListboardView(ExportViewMixin, CohortCHEUSwitchViewMixin,
                 {'subject_identifier': kwargs.get('subject_identifier')})
         return options
 
+    def get_queryset_exclude_options(self, request, *args, **kwargs):
+        """ Exclude participant's from a certain cohort, and exposure status
+            if the FUs are already full.
+        """
+        options = super().get_queryset_exclude_options(request, *args, **kwargs)
+        extra_options = self.fu_enrolment_limits_exclude
+        options.update(extra_options)
+        return options
+
+    @property
+    def fu_enrolment_limits_exclude(self):
+        """ Check if cohort enrolment FU limit has been reached and exclude
+            the specific exposure group per cohort from list of FUs available
+            for scheduling.
+        """
+        exclude_status = []
+        cohort_limits = {'cohort_b': [('EXPOSED', 200), ('UNEXPOSED', 100)],
+                         'cohort_c': [('EXPOSED', 100), ('UNEXPOSED', 200)]}
+
+        filter_options = self.get_queryset_filter_options(
+            self.request, *self.args, **self.kwargs)
+
+        cohort_name = filter_options.get('name', None)
+        limits = cohort_limits.get(cohort_name, [])
+
+        schedule_names = self.get_fu_schedule_names(cohort_name)
+        child_idx = self.subject_schedule_history_cls.objects.exclude(
+            subject_identifier__in=self.child_offstudy_pids).filter(
+            schedule_name__in=schedule_names).values_list(
+                'subject_identifier', flat=True)
+        for limit in limits:
+            exposure_status, _count = limit
+            limit_childidx = self.model_cls.objects.filter(
+                subject_identifier__in=child_idx,
+                exposure_status=exposure_status).values_list(
+                    'subject_identifier', flat=True)
+            limit_childidx = set(limit_childidx)
+            if len(limit_childidx) >= _count:
+                exclude_status.append(exposure_status)
+        return {'exposure_status__in': exclude_status}
+
     @property
     def export_fields(self):
         return ['subject_identifier', 'name', 'exposure_status',
